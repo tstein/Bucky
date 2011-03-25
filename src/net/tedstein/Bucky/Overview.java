1da0aca35cc797f4ffc873a3a7322e5b9442ad79
Ted Stein <karamarisan@gmail.com>
2011-03-25 01:41:00 -0500

    Add UI support for deleting datasets (with confirmation dialog).

--- src/net/tedstein/Bucky/Overview.java
+++ src/net/tedstein/Bucky/Overview.java
@@ -3,18 +3,29 @@ package net.tedstein.Bucky;
 import net.tedstein.Bucky.util.DatapointAdder;
 import net.tedstein.Bucky.util.DatasetCursorAdapter;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Overview extends Activity {
     private static final String TAG = "Bucky";
@@ -44,6 +55,66 @@ public class Overview extends Activity {
             return super.onOptionsItemSelected(item);
         }
     }
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        // Retrieve the TextView of the selected item so we can set a useful title.
+        LinearLayout item_view = (LinearLayout)((AdapterContextMenuInfo)menuInfo).targetView;
+        TextView name_view = (TextView)item_view.getChildAt(0);
+        CharSequence name = name_view.getText();
+        menu.setHeaderTitle(name);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.overview_item_context, menu);
+    }
+
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterContextMenuInfo info = (AdapterContextMenuInfo)item.getMenuInfo();
+        TextView name_view = (TextView)((LinearLayout)info.targetView).getChildAt(0);
+        final CharSequence name = name_view.getText();
+        final int set_id = (Integer)info.targetView.getTag();
+
+        final Context context = this;
+        final ContentResolver cr = getContentResolver();
+
+        switch (item.getItemId()) {
+        case R.id.overview_item_context_delete:
+            new AlertDialog.Builder(this)
+                .setTitle(getString(R.string.dataset_delete_prompt, name))
+                .setPositiveButton(getString(R.string.confirm), new OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Delete all points that belong to the given set.
+                        cr.delete(BuckyProvider.DATAPOINTS_URI,
+                                BuckyProvider.DP_SETID + "=?",
+                                new String[] {
+                                    String.valueOf(set_id) });
+
+                        // Then, delete the set.
+                        Uri datapoint_uri = Uri.withAppendedPath(
+                                BuckyProvider.DATASETS_URI,
+                                String.valueOf(set_id));
+                        cr.delete(datapoint_uri, null, null);
+
+                        Toast.makeText(context,
+                                getString(R.string.dataset_delete_prompt, name),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton(getString(R.string.decline), null)
+                .show();
+            return true;
+        default:
+            return super.onContextItemSelected(item);
+        } 
+    }
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -90,5 +161,6 @@ public class Overview extends Activity {
                 startActivity(i);
             }
         });
+        registerForContextMenu(setlist);
     }
 }
